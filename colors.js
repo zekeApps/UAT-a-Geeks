71eb3fa8350d9b65f29c85a5fc5177b89cb6a813
zekeApps <sheqel@ive.com>
2015-05-13 17:50:54 -0500

    colors.js added, contain the colorMatrix

---
+++ colors.js
@@ -0,0 +1,25 @@
+// This is the colorMatrix
+const colorMatrix = {
+  off: "#000000",
+  lightred: "#180000",
+  lightgreen: "#001800",
+  lightblue: "#000018",
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  turquoise: "#00ff55",
+  cyan: "#00ffff",
+  white: "#553355",
+  brightwhite: "#ffffff",
+  purple: "#3a0077",
+  magenta: "#ff00ff",
+  pink: "#ff0011",
+  yellow: "#e42500",
+  orange: "#ff0C00",
+  candy: "#ff0103"
+
+  }
+
+  module.exports = colorMatrix;
+
+}

ccb01089e9909727878ee089d29be4d3f0293f16
zekeApps <sheqel@ive.com>
2015-05-13 00:43:02 -0500

    blink.js example added

---
+++ blink.js
@@ -0,0 +1,10 @@
+//This is the Hello World equivalent using a LED
+
+const five = require("johnny-five")
+const board = new five.Board()
+
+board.on("ready", function(){
+  var led = new five.Led(13)
+
+  led.blink(200)
+})

4bd71e491f6a4e2f45e7446040205701e9f9d70a
zekeApps <sheqel@ive.com>
2015-05-14 00:15:52 -0500

    node-tweet-stream imported and simple Twitter Stream test perfomed

--- rainbow.js
+++ rainbow.js
@@ -1,5 +1,6 @@
 const five = require("johnny-five")
 const colorMatrix = require("./colors")
+const twit = require("node-tweet-stream")
 const board = new five.Board()
 
 board.on("ready", function () {
@@ -13,6 +14,25 @@ board.on("ready", function () {
   })
   led.color(colorMatrix["candy"])
   console.log("Arduino is Alive")
+
+
+
+  // Twitter OAuth
+  tw = new twit({
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    token: process.env.TWITTER_TOKEN,
+    token_secret: process.env.TWITTER_TOKEN_SECRET
+  })
+  tw.track("@zeke_glz")
+  tw.track("#UATaGeeks")
+  tw.on('tweet', function (tweet) {
+  console.log('tweet received', tweet)
+  })
+
+  tw.on('error', function (err) {
+  console.log('Oh no, error Occureed with Twitter Stream: ', err)
+  })
 
   this.repl.inject({
     led: led
